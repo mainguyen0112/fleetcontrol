bd21f4490299549e60de2d74efe347b4932b01a1
mainguyen0112 <nqmai12345678@gmail.com>
2026-06-27 22:11:48 +0700

    feat(user): add service layer with bcrypt password hashing

---
+++ api/internal/user/service.go
@@ -0,0 +1,43 @@
+package user
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) *Service {
+	return &Service{repo: repo}
+}
+
+func (s *Service) Create(ctx context.Context, username, password, role string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	u := &User{
+		Username:     username,
+		PasswordHash: string(hash),
+		Role:         role,
+	}
+
+	return s.repo.Create(ctx, u)
+}
+
+func (s *Service) GetByID(ctx context.Context, id uuid.UUID) (*User, error) {
+	return s.repo.GetByID(ctx, id)
+}
+
+func (s *Service) List(ctx context.Context) ([]*User, error) {
+	return s.repo.List(ctx)
+}
+
+func (s *Service) Delete(ctx context.Context, id uuid.UUID) error {
+	return s.repo.Delete(ctx, id)
+}
