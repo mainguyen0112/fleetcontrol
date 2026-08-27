6985fa58c8644bff4cd16be1a9850c346cdb2040
mainguyen0112 <nqmai12345678@gmail.com>
2026-08-27 11:18:30 +0700

    refactor(operator): normalize Satellite API group

--- operator/api/v1alpha1/groupversion_info.go
+++ operator/api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the fleetcontrol v1alpha1 API group.
 // +kubebuilder:object:generate=true
-// +groupName=fleetcontrol.fleetcontrol.io
+// +groupName=fleetcontrol.io
 package v1alpha1
 
 import (
@@ -28,7 +28,7 @@ import (
 var (
 	// SchemeGroupVersion is group version used to register these objects.
 	// This name is used by applyconfiguration generators (e.g. controller-gen).
-	SchemeGroupVersion = schema.GroupVersion{Group: "fleetcontrol.fleetcontrol.io", Version: "v1alpha1"}
+	SchemeGroupVersion = schema.GroupVersion{Group: "fleetcontrol.io", Version: "v1alpha1"}
 
 	// GroupVersion is an alias for SchemeGroupVersion, for backward compatibility.
 	GroupVersion = SchemeGroupVersion
--- operator/internal/controller/satellite_controller.go
+++ operator/internal/controller/satellite_controller.go
@@ -34,9 +34,9 @@ type SatelliteReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=fleetcontrol.fleetcontrol.io,resources=satellites,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=fleetcontrol.fleetcontrol.io,resources=satellites/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=fleetcontrol.fleetcontrol.io,resources=satellites/finalizers,verbs=update
+// +kubebuilder:rbac:groups=fleetcontrol.io,resources=satellites,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=fleetcontrol.io,resources=satellites/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=fleetcontrol.io,resources=satellites/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
